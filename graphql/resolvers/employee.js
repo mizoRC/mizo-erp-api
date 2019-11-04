7ea0f14f4a1749c70d802916e011737f6f280d9c
miguelrcDEV <mirodriguez11@gmail.com>
2019-11-04 23:42:58 +0100

    Update employee working

--- graphql/resolvers/employee.js
+++ graphql/resolvers/employee.js
@@ -33,13 +33,21 @@ const resolvers = {
                 const employee = await Employee.findOne({where: {email: updateInfo.email}});
                 const match = await bcrypt.compare(updateInfo.password, employee.password);
                 if(match && employee.active == 1){
-                    //UPDATE EMPLOYEE
+                    employee.name = updateInfo.name;
+                    employee.surname = updateInfo.surname;
+                    employee.language = updateInfo.language;
+                    if(!!updateInfo.newPassword && (updateInfo.newPassword === updateInfo.newPasswordRepeated)) {
+                        const hash = await bcrypt.hash(updateInfo.newPassword, 10);
+                        employee.password = hash;
+                    }
+                    
+                    const updatedEmployee = await employee.save();
 
-                    const token = jwt.sign({employee: employee}, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 8}); //16H
+                    const token = jwt.sign({employee: updatedEmployee}, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 8}); //16H
                     return { token };
                 }
                 else{
-                    throw new Error('Invalid employee');
+                    throw new Error('Password not match');
                 }
 			} catch (error) {
 				throw new Error(error);
