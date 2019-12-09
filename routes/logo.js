86e6bb45afa239494430f10df484fa5ab87f8d63
miguelrc <mirodriguez11@gmail.com>
2019-12-09 22:29:40 +0100

    Fix for company that has no logo

--- routes/logo.js
+++ routes/logo.js
@@ -12,14 +12,21 @@ router.get('/get/:employeeID', async (req, res) => {
         if(employee){
             const company = await employee.getCompany();
             const logo = company.logo;
-            const extension = logo.substring("data:".length, logo.indexOf(";base64"))
-            const b64Logo = logo.split(',')[1];
-            var img = new Buffer(b64Logo, 'base64');
-            res.writeHead(200, {
-                'Content-Type': extension,
-                'Content-Length': img.length 
-            });
-            res.end(img);
+            if(!!logo && logo !== ""){
+                const extension = logo.substring("data:".length, logo.indexOf(";base64"))
+                const b64Logo = logo.split(',')[1];
+                var img = new Buffer(b64Logo, 'base64');
+                res.writeHead(200, {
+                    'Content-Type': extension,
+                    'Content-Length': img.length 
+                });
+                res.end(img);
+            }
+            else{
+                const img = fs.readFileSync(path.resolve(__dirname, `../resources/logo_complete.png`));
+                res.writeHead(200, {'Content-Type': 'image/png' });
+                res.end(img, 'binary');
+            }
         }
         else{
             const img = fs.readFileSync(path.resolve(__dirname, `../resources/logo_complete.png`));
